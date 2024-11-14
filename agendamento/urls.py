56581acd90e524444c8f078a85e2f32068fcbe14
@Adilio89 <aamauricio2789@gmail.com>
2024-11-14 15:44:01 -0300

    Projeto Lava Rapido - Segundo Commit

--- agendamento/urls.py
+++ agendamento/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
     path('agendamentos-do-dia/', views.agendamentos_do_dia, name='agendamentos_do_dia'),
+    path('alterar-status/<int:agendamento_id>/<str:novo_status>/', views.alterar_status, name='alterar_status'),
 ]
--- agendamento/views.py
+++ agendamento/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.utils import timezone
+import datetime
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.decorators.http import require_POST
 from .models import Agendamento
 
 
@@ -8,7 +9,13 @@ def index(request):
 
 
 def agendamentos_do_dia(request):
-    data_atual = timezone.now().date()
-    agendamentos = Agendamento.objects.filter(data_agendamento__date = data_atual)
-
+    agendamentos = Agendamento.objects.filter(data_agendamento__date = datetime.date.today())
     return render(request, 'agendamento/agendamentos_do_dia.html', {'agendamentos': agendamentos})
+
+
+@require_POST
+def alterar_status(request, agendamento_id, novo_status):
+    agendamento = get_object_or_404(Agendamento, id=agendamento_id)
+    agendamento.status = novo_status
+    agendamento.save()
+    return redirect('agendamentos_do_dia')
